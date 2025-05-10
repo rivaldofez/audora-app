e4e7cc4b1cc5738aee395c7056c8b1af322a6af9
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-05-10 10:44:00 +0700

    [FEAT] Create custom search view

---
+++ Audora/Presentation/MusicPlayer/View/SearchView.swift
@@ -0,0 +1,41 @@
+//
+//  SearchView.swift
+//  Audora
+//
+//  Created by Rivaldo Fernandes on 23/03/25.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var placeholder: String = ""
+    @Binding var searchQuery: String
+    var onSubmitQuery: (String) -> Void
+    
+    var body: some View {
+        ZStack {
+            HStack {
+                TextField(placeholder, text: $searchQuery)
+                    .font(.custom(.rubik, size: 14))
+                    .padding()
+                    .onSubmit {}
+                
+                Button(action: {
+                    onSubmitQuery(searchQuery)
+                }, label: {
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(.black)
+                        .padding(.horizontal)
+                })
+            }
+        }
+        .background(.white)
+        .cornerRadius(10)
+    }
+}
+
+#Preview {
+    SearchView(placeholder: "Search music, album.....", searchQuery: .constant(""), onSubmitQuery: {_ in })
+}
