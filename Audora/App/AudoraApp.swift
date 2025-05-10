dae2a48e30b5f24ec8634747f4205b6e63ef37c2
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-05-10 10:36:00 +0700

    [FEAT] Implement injection

--- Audora/App/AudoraApp.swift
+++ Audora/App/AudoraApp.swift
@@ -11,6 +11,10 @@ import SwiftUI
 struct AudoraApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
+    init(){
+        DIContainer.shared.registration()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
