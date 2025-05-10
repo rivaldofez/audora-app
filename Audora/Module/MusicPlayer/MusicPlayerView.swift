3e3b2d4e793ac201638989c5b84912d3b8dd3651
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-05-10 11:40:00 +0700

    [FIX] Remove bug and change folder group

--- Audora/Module/MusicPlayer/MusicPlayerView.swift
+++ Audora/Module/MusicPlayer/MusicPlayerView.swift
@@ -100,23 +100,3 @@ struct MusicPlayerView: View {
 #Preview {
     MusicPlayerView()
 }
-
-struct RoundedCornersShape: Shape {
-    var corners: UIRectCorner
-    var radius: CGFloat
-    
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(
-            roundedRect: rect,
-            byRoundingCorners: corners,
-            cornerRadii: CGSize(width: radius, height: radius)
-        )
-        return Path(path.cgPath)
-    }
-}
-
-extension AnyTransition {
-    static var slideUp: AnyTransition {
-        .move(edge: .bottom).combined(with: .opacity)
-    }
-}
