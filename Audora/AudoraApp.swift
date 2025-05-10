7912eff225431babf4784fbe5e12e41a20459a29
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-05-10 09:04:00 +0700

    [FEAT] Initialize project

---
+++ Audora/AudoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudoraApp.swift
+//  Audora
+//
+//  Created by Rivaldo Fernandes on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AudoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
